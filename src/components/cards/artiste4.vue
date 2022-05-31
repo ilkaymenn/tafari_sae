bc2b312c352a97121147f71ce3c60e4fe587d25c
ilkaymenn <ilkay.men@edu.univ-fcomte.fr>
2022-05-31 14:29:19 +0200

    prblm nom image

--- src/components/cards/artiste4.vue
+++ src/components/cards/artiste4.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="w-64">
-    <img src="../../assets/eddy grant.jpg" alt="photographie de Eddy Grant" />
+    <img src="../../assets/eddy-grant.jpg" alt="photographie de Eddy Grant" />
     <div class="h-20 rounded-br-3xl bg-rouge p-2">
       <h3 class="font-prompt text-3xl font-bold uppercase text-blanc">
         eddy grant
