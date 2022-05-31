9f8276412bfddddd7d66ccf34571a38d63bc55a3
ilkaymenn <ilkay.men@edu.univ-fcomte.fr>
2022-05-31 15:02:26 +0200

    festival fini

--- src/views/festivalView.vue
+++ src/views/festivalView.vue
@@ -14,7 +14,7 @@
         </h2>
       </div>
       <div class="m-8 lg:grid lg:grid-cols-2 lg:justify-items-center">
-        <p class="font-spinnaker text-lg text-blanc lg:ml-10 lg:mt-5">
+        <p class="font-spinnaker text-lg text-blanc lg:ml-16 lg:mt-5">
           Lorem ipsum dolor sit amet. Et repudiandae doloribus ea quibusdam
           rerum ut error quae ut voluptas autem et neque saepe est neque
           corrupti. Eos earum rerum qui ipsa asperiores non atque quis eum
@@ -35,7 +35,7 @@
     <div class="border-t-2 border-noir bg-rouge">
       <div class="m-10 lg:grid lg:grid-cols-3">
         <p
-          class="font-spinnaker text-lg text-blanc lg:col-span-2 lg:col-start-2 lg:mt-20"
+          class="font-spinnaker text-lg text-blanc lg:col-span-2 lg:col-start-2 lg:ml-20 lg:mr-28 lg:mt-20"
         >
           Lorem ipsum dolor sit amet. Et repudiandae doloribus ea quibusdam
           rerum ut error quae ut voluptas autem et neque saepe est neque
@@ -50,7 +50,7 @@
         <img
           src="../assets/aditya-chinchure-ZhQCZjr9fHo-unsplash.jpg"
           alt="photographie de festival"
-          class="mt-5 w-96 rounded-xl border-2 border-noir lg:-mt-40 lg:ml-20"
+          class="mt-5 w-96 rounded-xl border-2 border-noir lg:-mt-52 lg:ml-20"
         />
       </div>
     </div>
