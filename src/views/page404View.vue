445b6215152bee726b3fc154242399c786470709
ilkaymenn <ilkay.men@edu.univ-fcomte.fr>
2022-06-01 23:07:29 +0200

    page 404 ok

--- src/views/page404View.vue
+++ src/views/page404View.vue
@@ -1,28 +1,14 @@
 <template>
   <div
-    class="h-screen bg-gradient-to-b from-vert via-jaune to-rouge text-center"
+    class="-mt-16 grid h-screen place-items-center bg-gradient-to-b from-vert via-jaune to-rouge text-center lg:pt-20"
   >
-    <h2 class="text-9xl text-noir">404</h2>
-    <p class="text-7xl capitalize text-noir">page not found</p>
-    <iframe
-      src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d87209.26948551332!2d6.3095785370156126!3d46.916686593334184!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x478dbcc6241cf761%3A0x7758320cd6d19236!2s25300%20Pontarlier!5e0!3m2!1sfr!2sfr!4v1654075539660!5m2!1sfr!2sfr"
-      width="600"
-      height="450"
-      style="border: 0"
-      allowfullscreen=""
-      loading="lazy"
-      referrerpolicy="no-referrer-when-downgrade"
-    ></iframe>
-
-    <img
-      src="../../public/rasta.png"
-      alt=""
-      class="m-10 mb-10 h-10 animate-bounce"
-    />
-    <button
-      class="mt-20 mb-20 bg-noir text-blanc transition duration-150 ease-out hover:ease-in"
-    >
-      Essaie
-    </button>
+    <div class="">
+      <h2 class="text-5xl font-semibold text-noir">oups</h2>
+      <h3 class="text-9xl font-semibold capitalize text-noir">404</h3>
+      <p class="text-xl font-semibold capitalize text-noir">
+        la page que vous recherchez semble introuvable
+      </p>
+    </div>
+    <img src="../../public/icon-404.png" alt="" class="-mt-52 lg:-mt-2" />
   </div>
 </template>
