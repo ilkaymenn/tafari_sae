a30aabc0b760f09018d9b237a4042a52a9b31aa9
ilkaymenn <ilkay.men@edu.univ-fcomte.fr>
2022-06-02 01:41:42 +0200

    mi page artiste manu

--- src/views/manuchaoView.vue
+++ src/views/manuchaoView.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="mb-10">
-    <div>
+  <div>
+    <div class="">
       <div class="mt-4">
         <img
           src="../../public/icon-rasta.png"
@@ -17,35 +17,55 @@
         </div>
       </div>
     </div>
-    <div class="mt-8 grid place-content-end">
-      <img src="../../public/manu-chao.jpg" alt="" />
-    </div>
-    <div class="mt-5 mr-8 bg-gradient-to-b from-vert via-jaune to-rouge p-3">
-      <p class="font-spinnaker text-base text-noir">
-        Lorem ipsum dolor sit amet. Et repudiandae doloribus ea quibusdam rerum
-        ut error quae ut voluptas autem et neque saepe est neque corrupti. Eos
-        earum rerum qui ipsa asperiores non atque quis eum ducimus sint est
-        suscipit magnam qui natus impedit qui debitis exercitationem. Eos quae
-        ratione quo vero molestiae ut rerum architecto? Id voluptas expedita id
-        neque quam qui consequuntur deleniti est exercitationem distinctio qui
-        totam adipisci. Quo molestias quos in perferendis quidem ea accusantium
-        temporibus quo unde dolor qui libero voluptatibus.
-      </p>
-    </div>
-    <div class="mt-6 grid place-content-end">
-      <img src="../../public/manu-chao.jpg" alt="" />
-    </div>
-    <div class="mt-5 mr-8 bg-gradient-to-b from-vert via-jaune to-rouge p-3">
-      <p class="text-nase font-spinnaker text-noir">
-        Lorem ipsum dolor sit amet. Et repudiandae doloribus ea quibusdam rerum
-        ut error quae ut voluptas autem et neque saepe est neque corrupti. Eos
-        earum rerum qui ipsa asperiores non atque quis eum ducimus sint est
-        suscipit magnam qui natus impedit qui debitis exercitationem. Eos quae
-        ratione quo vero molestiae ut rerum architecto? Id voluptas expedita id
-        neque quam qui consequuntur deleniti est exercitationem distinctio qui
-        totam adipisci. Quo molestias quos in perferendis quidem ea accusantium
-        temporibus quo unde dolor qui libero voluptatibus.
-      </p>
+    <div class="mb-10">
+      <div class="mb-10">
+        <div class="mt-8 grid place-content-end">
+          <img
+            src="../../public/manu-chao.jpg"
+            alt=""
+            class="lg:mr-40 lg:h-96"
+          />
+        </div>
+        <div
+          class="mt-5 mr-8 bg-gradient-to-b from-vert via-jaune to-rouge p-3 lg:ml-28 lg:-mt-80 lg:w-96"
+        >
+          <p class="font-spinnaker text-base text-noir">
+            Lorem ipsum dolor sit amet. Et repudiandae doloribus ea quibusdam
+            rerum ut error quae ut voluptas autem et neque saepe est neque
+            corrupti. Eos earum rerum qui ipsa asperiores non atque quis eum
+            ducimus sint est suscipit magnam qui natus impedit qui debitis
+            exercitationem. Eos quae ratione quo vero molestiae ut rerum
+            architecto? Id voluptas expedita id neque quam qui consequuntur
+            deleniti est exercitationem distinctio qui totam adipisci. Quo
+            molestias quos in perferendis quidem ea accusantium temporibus quo
+            unde dolor qui libero voluptatibus.
+          </p>
+        </div>
+      </div>
+      <div class="lg:grid-cols-4">
+        <div class="mt-6 grid place-content-end">
+          <img
+            src="../../public/manu-chao.jpg"
+            alt=""
+            class="lg:mt-10 lg:h-96"
+          />
+        </div>
+        <div
+          class="mt-5 mr-8 bg-gradient-to-b from-vert via-jaune to-rouge p-3 lg:w-80"
+        >
+          <p class="text-nase font-spinnaker text-noir">
+            Lorem ipsum dolor sit amet. Et repudiandae doloribus ea quibusdam
+            rerum ut error quae ut voluptas autem et neque saepe est neque
+            corrupti. Eos earum rerum qui ipsa asperiores non atque quis eum
+            ducimus sint est suscipit magnam qui natus impedit qui debitis
+            exercitationem. Eos quae ratione quo vero molestiae ut rerum
+            architecto? Id voluptas expedita id neque quam qui consequuntur
+            deleniti est exercitationem distinctio qui totam adipisci. Quo
+            molestias quos in perferendis quidem ea accusantium temporibus quo
+            unde dolor qui libero voluptatibus.
+          </p>
+        </div>
+      </div>
     </div>
   </div>
 </template>
