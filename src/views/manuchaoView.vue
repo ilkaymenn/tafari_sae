2235571f2157f4aa7d5bb0abc60b411a8a4f532b
ilkaymenn <ilkay.men@edu.univ-fcomte.fr>
2022-06-02 00:05:20 +0200

    base manu chao

--- src/views/manuchaoView.vue
+++ src/views/manuchaoView.vue
@@ -1,79 +1,30 @@
 <template>
-  <div
-    id="carouselExampleIndicators"
-    class="carousel slide relative"
-    data-bs-ride="carousel"
-  >
-    <div
-      class="carousel-indicators absolute right-0 bottom-0 left-0 mb-4 flex justify-center p-0"
-    >
-      <button
-        type="button"
-        data-bs-target="#carouselExampleIndicators"
-        data-bs-slide-to="0"
-        class="active"
-        aria-current="true"
-        aria-label="Slide 1"
-      ></button>
-      <button
-        type="button"
-        data-bs-target="#carouselExampleIndicators"
-        data-bs-slide-to="1"
-        aria-label="Slide 2"
-      ></button>
-      <button
-        type="button"
-        data-bs-target="#carouselExampleIndicators"
-        data-bs-slide-to="2"
-        aria-label="Slide 3"
-      ></button>
+  <div>
+    <div>
+      <h2></h2>
+      <p></p>
     </div>
-    <div class="carousel-inner relative w-full overflow-hidden">
-      <div class="carousel-item active float-left w-full">
-        <img
-          src="https://mdbcdn.b-cdn.net/img/new/slides/041.webp"
-          class="block w-full"
-          alt="Wild Landscape"
-        />
-      </div>
-      <div class="carousel-item float-left w-full">
-        <img
-          src="https://mdbcdn.b-cdn.net/img/new/slides/042.webp"
-          class="block w-full"
-          alt="Camera"
-        />
-      </div>
-      <div class="carousel-item float-left w-full">
-        <img
-          src="https://mdbcdn.b-cdn.net/img/new/slides/043.webp"
-          class="block w-full"
-          alt="Exotic Fruits"
-        />
-      </div>
+    <div>
+      <img
+        src="../../public/icon-rasta.png"
+        alt=""
+        class="m-10 mb-10 h-10 animate-bounce"
+      />
+      <img src="" alt="" />
     </div>
-    <button
-      class="carousel-control-prev absolute top-0 bottom-0 left-0 flex items-center justify-center border-0 p-0 text-center hover:no-underline hover:outline-none focus:no-underline focus:outline-none"
-      type="button"
-      data-bs-target="#carouselExampleIndicators"
-      data-bs-slide="prev"
-    >
-      <span
-        class="carousel-control-prev-icon inline-block bg-no-repeat"
-        aria-hidden="true"
-      ></span>
-      <span class="visually-hidden">Previous</span>
-    </button>
-    <button
-      class="carousel-control-next absolute top-0 bottom-0 right-0 flex items-center justify-center border-0 p-0 text-center hover:no-underline hover:outline-none focus:no-underline focus:outline-none"
-      type="button"
-      data-bs-target="#carouselExampleIndicators"
-      data-bs-slide="next"
-    >
-      <span
-        class="carousel-control-next-icon inline-block bg-no-repeat"
-        aria-hidden="true"
-      ></span>
-      <span class="visually-hidden">Next</span>
-    </button>
+    <div>
+      <p></p>
+    </div>
+    <div>
+      <img
+        src="../../public/icon-rasta.png"
+        alt=""
+        class="m-10 mb-10 h-10 animate-bounce"
+      />
+      <img src="" alt="" />
+    </div>
+    <div>
+      <p></p>
+    </div>
   </div>
 </template>
