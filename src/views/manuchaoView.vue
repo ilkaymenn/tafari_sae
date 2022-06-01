0316d51803f8869105c295a2c46e31e90be158bf
ilkaymenn <ilkay.men@edu.univ-fcomte.fr>
2022-06-02 00:34:07 +0200

    article manu mobile

--- src/views/manuchaoView.vue
+++ src/views/manuchaoView.vue
@@ -1,30 +1,51 @@
 <template>
-  <div>
+  <div class="mb-10">
     <div>
-      <h2></h2>
-      <p></p>
+      <div class="mt-4">
+        <img
+          src="../../public/icon-rasta.png"
+          alt=""
+          class="ml-36 -mb-8 h-14 animate-bounce"
+        />
+        <div class="-mt-6 w-fit bg-noir p-3">
+          <h2 class="font-reggae-by-aslam text-2xl capitalize text-blanc">
+            manu chao
+          </h2>
+          <p class="font-roboto text-lg text-blanc text-opacity-50">
+            Jour 1 ¦ 18h00
+          </p>
+        </div>
+      </div>
     </div>
-    <div>
-      <img
-        src="../../public/icon-rasta.png"
-        alt=""
-        class="m-10 mb-10 h-10 animate-bounce"
-      />
-      <img src="" alt="" />
+    <div class="mt-8 grid place-content-end">
+      <img src="../../public/manu-chao.jpg" alt="" />
     </div>
-    <div>
-      <p></p>
+    <div class="mt-5 mr-8 bg-gradient-to-b from-vert via-jaune to-rouge p-3">
+      <p class="font-spinnaker text-base text-noir">
+        Lorem ipsum dolor sit amet. Et repudiandae doloribus ea quibusdam rerum
+        ut error quae ut voluptas autem et neque saepe est neque corrupti. Eos
+        earum rerum qui ipsa asperiores non atque quis eum ducimus sint est
+        suscipit magnam qui natus impedit qui debitis exercitationem. Eos quae
+        ratione quo vero molestiae ut rerum architecto? Id voluptas expedita id
+        neque quam qui consequuntur deleniti est exercitationem distinctio qui
+        totam adipisci. Quo molestias quos in perferendis quidem ea accusantium
+        temporibus quo unde dolor qui libero voluptatibus.
+      </p>
     </div>
-    <div>
-      <img
-        src="../../public/icon-rasta.png"
-        alt=""
-        class="m-10 mb-10 h-10 animate-bounce"
-      />
-      <img src="" alt="" />
+    <div class="mt-6 grid place-content-end">
+      <img src="../../public/manu-chao.jpg" alt="" />
     </div>
-    <div>
-      <p></p>
+    <div class="mt-5 mr-8 bg-gradient-to-b from-vert via-jaune to-rouge p-3">
+      <p class="text-nase font-spinnaker text-noir">
+        Lorem ipsum dolor sit amet. Et repudiandae doloribus ea quibusdam rerum
+        ut error quae ut voluptas autem et neque saepe est neque corrupti. Eos
+        earum rerum qui ipsa asperiores non atque quis eum ducimus sint est
+        suscipit magnam qui natus impedit qui debitis exercitationem. Eos quae
+        ratione quo vero molestiae ut rerum architecto? Id voluptas expedita id
+        neque quam qui consequuntur deleniti est exercitationem distinctio qui
+        totam adipisci. Quo molestias quos in perferendis quidem ea accusantium
+        temporibus quo unde dolor qui libero voluptatibus.
+      </p>
     </div>
   </div>
 </template>
