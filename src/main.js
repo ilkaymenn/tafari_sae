f647903a474c3c68e605fe4b4f70a4e0da4d0e31
ilkaymenn <ilkay.men@edu.univ-fcomte.fr>
2022-06-02 09:22:49 +0200

    mon compte base ok

--- src/main.js
+++ src/main.js
@@ -1,12 +1,35 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-import './index.css'
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
+import "./index.css";
 
-createApp(App).mount('#app') /*burasi ok mi ?*/
+// Import de mitt
+import mitt from "mitt";
 
-const app = createApp(App)
+// Import fonction d'initialisation du SDK Firebase
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.7.0/firebase-app.js";
 
-app.use(router)
+// Votre configuration de Firebase
+const firebaseConfig = {
+  apiKey: "AIzaSyDKzixo__x2BJOXduGZ9JsZ71LIKLhmr-4",
+  authDomain: "tafari-sae.firebaseapp.com",
+  projectId: "tafari-sae",
+  storageBucket: "tafari-sae.appspot.com",
+  messagingSenderId: "745884367346",
+  appId: "1:745884367346:web:e506c3ffdf08d0db12536b",
+};
 
-app.mount('#app')
+// Initialize Firebase
+const appFirebase = initializeApp(firebaseConfig);
+
+const app = createApp(App);
+
+// Création d'un emetteur mitt exportable
+export const emitter = mitt();
+// créer l'émetteur comme propriété globale
+// de l'application
+app.config.globalProperties.emitter = emitter;
+
+app.use(router);
+
+app.mount("#app");
