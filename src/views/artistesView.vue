61c3594cc099c499154dc2072dbeccf0181c4042
ilkaymenn <ilkay.men@edu.univ-fcomte.fr>
2022-06-02 12:01:15 +0200

    je sais pas où j'en suis

--- src/views/artistesView.vue
+++ src/views/artistesView.vue
@@ -1,175 +1,177 @@
 <template>
-  <div class="mb-16">
-    <div
-      class="mt-10 grid justify-items-center lg:ml-10 lg:justify-items-start"
-    >
-      <div class="bg-gradient-to-br from-vert via-jaune to-rouge p-2">
-        <h2 class="font-reggae-by-aslam text-4xl capitalize text-blanc">
-          les artistes
-        </h2>
-        <h2
-          class="-mt-9 ml-0.5 font-reggae-by-aslam text-4xl capitalize text-noir"
+  <div class="container">
+    <div class="card-header">
+      <h5>Liste des catégories</h5>
+    </div>
+    <hr />
+
+    <form>
+      <h6>Nouvelle catégorie</h6>
+      <div class="input-group">
+        <div class="input-group-prepend">
+          <span class="input-group-text">Libellé</span>
+        </div>
+        <input type="text" class="form-control" v-model="nom" required />
+        <button
+          class="btn btn-light"
+          type="button"
+          @click="createArtiste()"
+          title="Création"
         >
-          les artistes
-        </h2>
+          dlkgndgb
+        </button>
       </div>
+    </form>
+
+    <div class="card-body table-responsive">
+      <table class="text-light table">
+        <thead>
+          <tr>
+            <th scope="col">
+              <div class="float-left">Liste des catégories actuelles</div>
+              <span class="float-right">
+                <div class="input-group">
+                  <div class="input-group-prepend">
+                    <span class="input-group-text">Filtrage</span>
+                  </div>
+                  <input type="text" class="form-control" v-model="filter" />
+                  <button class="btn btn-light" type="button" title="Filtrage">
+                    sfbsbf
+                  </button>
+                </div>
+              </span>
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="artiste in filterByNom" :key="artiste.id">
+            <td>
+              <form>
+                <div class="input-group">
+                  <div class="input-group-prepend">
+                    <span class="input-group-text">Libellé</span>
+                  </div>
+                  <input
+                    type="text"
+                    class="form-control"
+                    v-model="artiste.nom"
+                    required
+                  />
+                  <button
+                    class="btn btn-light"
+                    type="submit"
+                    @click.prevent="updateArtiste(artiste)"
+                    title="Modification"
+                  >
+                    <i class="fa fa-save fa-lg"></i>
+                  </button>
+                  <button
+                    class="btn btn-light"
+                    type="submit"
+                    @click.prevent="deleteArtiste(artiste)"
+                    title="Suppression"
+                  >
+                    delete
+                  </button>
+                </div>
+              </form>
+            </td>
+          </tr>
+        </tbody>
+      </table>
     </div>
-    <div
-      class="grid sm:grid sm:grid-cols-2 md:grid md:grid-cols-3 lg:grid lg:grid-cols-4"
-    >
-      <div>
-        <card
-          class="bg-rouge"
-          image="../../alpha-blondy.jpg"
-          alt="Photographie de Alpha Blondy"
-          artiste="Alpha Blondy"
-          jour="jour 1"
-        />
-        <RouterLink to="/pageconstruction">
-          <div class="ml-80 -mt-16">
-            <button
-              class="rounded-full bg-jaune hover:border-2 hover:border-noir hover:bg-gradient-to-b hover:from-vert hover:via-jaune hover:to-rouge"
-            >
-              <PlusIcon class="h-10" />
-            </button></div
-        ></RouterLink>
-      </div>
-      <div>
-        <card
-          class="bg-vert"
-          image="../../damian-marley.jpg"
-          alt="Photographie de Damian Marley"
-          artiste="Damian Marley"
-          jour="jour 2"
-        />
-        <RouterLink to="/pageconstruction">
-          <div class="ml-80 -mt-16">
-            <button
-              class="rounded-full bg-jaune hover:border-2 hover:border-noir hover:bg-gradient-to-b hover:from-vert hover:via-jaune hover:to-rouge"
-            >
-              <PlusIcon class="h-10" />
-            </button></div
-        ></RouterLink>
-      </div>
-      <div>
-        <card
-          class="bg-rouge"
-          image="../../eddy-grant.jpg"
-          alt="Photographie de Eddy Grant"
-          artiste="Eddy Grant"
-          jour="jour 1"
-        />
-        <RouterLink to="/pageconstruction">
-          <div class="ml-80 -mt-16">
-            <button
-              class="rounded-full bg-jaune hover:border-2 hover:border-noir hover:bg-gradient-to-b hover:from-vert hover:via-jaune hover:to-rouge"
-            >
-              <PlusIcon class="h-10" />
-            </button></div
-        ></RouterLink>
-      </div>
-      <div>
-        <card
-          class="bg-vert"
-          image="../../horace-andy.jpg"
-          alt="Photographie de Horace Andy"
-          artiste="Horace Andy"
-          jour="jour 2"
-        />
-        <RouterLink to="/pageconstruction">
-          <div class="ml-80 -mt-16">
-            <button
-              class="rounded-full bg-jaune hover:border-2 hover:border-noir hover:bg-gradient-to-b hover:from-vert hover:via-jaune hover:to-rouge"
-            >
-              <PlusIcon class="h-10" />
-            </button></div
-        ></RouterLink>
-      </div>
-      <div>
-        <card
-          class="bg-rouge"
-          image="../../public/manu-chao.jpg"
-          alt="Photographie de Manu Chao"
-          artiste="Manu Chao"
-          jour="jour 1"
-        />
-        <RouterLink to="/manuchao">
-          <div class="ml-80 -mt-16">
-            <button
-              class="rounded-full bg-jaune hover:border-2 hover:border-noir hover:bg-gradient-to-b hover:from-vert hover:via-jaune hover:to-rouge"
-            >
-              <PlusIcon class="h-10" />
-            </button></div
-        ></RouterLink>
-      </div>
-      <div>
-        <card
-          class="bg-vert"
-          image="../../pierpoljak.jpg"
-          alt="Photographie de Pierpoljak"
-          artiste="Pierpoljak"
-          jour="jour 2"
-        />
-        <RouterLink to="/pageconstruction">
-          <div class="ml-80 -mt-16">
-            <button
-              class="rounded-full bg-jaune hover:border-2 hover:border-noir hover:bg-gradient-to-b hover:from-vert hover:via-jaune hover:to-rouge"
-            >
-              <PlusIcon class="h-10" />
-            </button></div
-        ></RouterLink>
-      </div>
-      <div>
-        <card
-          class="bg-rouge"
-          image="../../rita-marley.jpg"
-          alt="Photographie de Rita Marley"
-          artiste="Rita Marley"
-          jour="jour 1"
-        />
-        <RouterLink to="/pageconstruction">
-          <div class="ml-80 -mt-16">
-            <button
-              class="rounded-full bg-jaune hover:border-2 hover:border-noir hover:bg-gradient-to-b hover:from-vert hover:via-jaune hover:to-rouge"
-            >
-              <PlusIcon class="h-10" />
-            </button></div
-        ></RouterLink>
-      </div>
-      <div>
-        <card
-          class="bg-vert"
-          image="../../yaniss-odua.jpg"
-          alt="Photographie de Yaniss Odua"
-          artiste="Yaniss Odua"
-          jour="jour 2"
-        />
-        <RouterLink to="/pageconstruction">
-          <div class="ml-80 -mt-16">
-            <button
-              class="rounded-full bg-jaune hover:border-2 hover:border-noir hover:bg-gradient-to-b hover:from-vert hover:via-jaune hover:to-rouge"
-            >
-              <PlusIcon class="h-10" />
-            </button></div
-        ></RouterLink>
-      </div>
-    </div>
-    <RouterView />
   </div>
 </template>
 
 <script>
-import card from "../components/cards/card.vue";
-import { PlusIcon } from "@heroicons/vue/outline";
+import {
+  getFirestore,
+  collection,
+  doc,
+  getDocs,
+  addDoc,
+  updateDoc,
+  deleteDoc,
+  onSnapshot,
+} from "https://www.gstatic.com/firebasejs/9.7.0/firebase-firestore.js";
 
 export default {
-  name: "App",
-  components: {
-    card,
-    PlusIcon,
-  },
+  name: "ListeView",
   data() {
-    return {};
+    return {
+      nom: null, // Pour la création d'une catégorie
+      listeArtisteSynchro: [],
+      filter: "",
+    };
+  },
+
+  computed: {
+    //Tri des catégories par ordre alpha
+    orderByNom: function () {
+      return this.listeArtisteSynchro.sort(function (a, b) {
+        if (a.nom < b.nom) return -1;
+        if (a.nom > b.nom) return 1;
+        return 0;
+      });
+    },
+
+    filterByNom: function () {
+      if (this.filter.length > 0) {
+        let filter = this.filter.toLowerCase();
+        return this.orderByNom.filter(function (artiste) {
+          return artiste.nom.toLowerCase().includes(filter);
+        });
+      } else {
+        return this.orderByNom;
+      }
+    },
+  },
+
+  mounted() {
+    this.getArtisteSynchro();
+  },
+  methods: {
+    async getArtisteSynchro() {
+      // Obtenir Firestore
+      const firestore = getFirestore();
+      // Base de données (collection)  document pays
+      const dbArtiste = collection(firestore, "artiste");
+      // Liste des pays synchronisée
+      const query = await onSnapshot(dbArtiste, (snapshot) => {
+        //  Récupération des résultats dans listePaysSynchro
+        // On utilse map pour récupérer l'intégralité des données renvoyées
+        // on identifie clairement le id du document
+        // les rest parameters permet de préciser la récupération de toute la partie data
+        this.listeArtisteSynchro = snapshot.docs.map((doc) => ({
+          id: doc.id,
+          ...doc.data(),
+        }));
+      });
+    },
+    async createArtiste() {
+      const firestore = getFirestore();
+      const dbArtiste = collection(firestore, "artiste");
+      const docRef = await addDoc(dbArtiste, {
+        nom: this.nom,
+      });
+      console.log("document crée avec le id : ", docRef.id);
+    },
+
+    async updateArtiste(artiste) {
+      const firestore = getFirestore();
+      const docRef = doc(firestore, "artiste", artiste.id);
+      await updateDoc(docRef, {
+        nom: artiste.nom,
+      });
+    },
+
+    async deleteArtiste(artiste) {
+      const firestore = getFirestore();
+      const docRef = doc(firestore, "artiste", artiste.id);
+      await deleteDoc(docRef);
+    },
   },
 };
 </script>
+
+<style scoped></style>
